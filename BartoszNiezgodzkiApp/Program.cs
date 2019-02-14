dde920e4737f03b1708618efce1b55b686ddc497
Bartosz Niezgodzki <niezgodzkibartosz@gmail.com>
2019-02-14 21:10:11 +0100

    after first quest

---
+++ BartoszNiezgodzkiApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace BartoszNiezgodzkiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string readText = File.ReadAllText("tea-data.txt");
+            string[] splitText = readText.Split('\n');
+            string[] splitTextReverse = splitText;
+            Array.Reverse(splitTextReverse);
+            File.WriteAllLines("result-1.txt", splitTextReverse);
+            foreach (string line in splitTextReverse)
+            {
+                Console.WriteLine(line);
+            }
+
+            List<Tea> teaList = new List<Tea>();
+
+            foreach (string line in splitText)
+            {
+                string[] splitLine = line.Split(',');
+                
+                teaList.Add(new Tea() { Name = splitLine[0], Kind = splitLine[1], BrewingTemperature = splitLine[2], BrewingTime = splitLine[3], Note = splitLine[3]  } );
+            }
+
+            Console.ReadKey();
+
+
+        }
+
+    }
+
+
+       
+   }
---
+++ BartoszNiezgodzkiApp/Tea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BartoszNiezgodzkiApp
+{
+    public class Tea
+    {
+        public string Name { get; set; }
+        public string Kind { get; set; }
+        public string BrewingTemperature { get; set; }
+        public string BrewingTime { get; set; }
+        public string Note { get; set; }
+    }
+}
