c42cefafc78e404dc6f837b6a9226fbdf5ddccd0
Bartosz Niezgodzki <niezgodzkibartosz@gmail.com>
2019-02-16 21:38:22 +0100

    po 3 zadanku

--- BartoszNiezgodzkiApp/Program.cs
+++ BartoszNiezgodzkiApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-
+using System.Linq;
 
 namespace BartoszNiezgodzkiApp
 {
@@ -12,30 +12,80 @@ namespace BartoszNiezgodzkiApp
 
             string readText = File.ReadAllText("tea-data.txt");
             string[] splitText = readText.Split('\n');
-            string[] splitTextReverse = splitText;
-            Array.Reverse(splitTextReverse);
-            File.WriteAllLines("result-1.txt", splitTextReverse);
-            foreach (string line in splitTextReverse)
+            string heading = splitText[0];
+
+
+            for (int i=0; ;i++)
             {
-                Console.WriteLine(line);
+                if (i >= 1)
+                {
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                Console.WriteLine("Which command do you wish to run? (1-6, 0-Exit) ");
+                int taskNumber = int.Parse(Console.ReadLine());
+                if (taskNumber == 0) break;
+                Console.Clear();
+
+                switch (taskNumber)
+                {
+
+                    case 1:
+
+                        string[] splitTextReverse = splitText;
+                        Array.Reverse(splitTextReverse);
+                        File.WriteAllLines("result-1.txt", splitTextReverse);
+
+                        foreach (string line in splitTextReverse)
+                        {
+                            Console.WriteLine(line);
+                        }
+
+                        break;
+
+                    case 2:
+
+                        List<Tea> teaList = Tea.WriteData(splitText);
+                        var sortedTeaList = from tea in teaList
+                                            orderby tea.Kind
+                                            select tea;
+
+                        string resultTextCommand_2 = heading  + "\n" + Environment.NewLine;
+
+                        foreach (Tea tea in sortedTeaList)
+                        {
+                            resultTextCommand_2 += tea.Name + ", " + tea.Kind + ", " + tea.BrewingTemperature + ", " + tea.BrewingTime + ", " + tea.SpecialFeature + "\n";
+                        }
+
+                        Console.WriteLine(resultTextCommand_2);
+                        File.WriteAllText("result-2.txt", resultTextCommand_2);
+
+                        break;
+
+                    case 3:
+                        break;
+                    case 4:
+                        Console.WriteLine("put the name of the tea to be used");
+                        string readName = Console.ReadLine();
+                        Console.WriteLine("put the temperature of water you want to use");
+                        int readTemperature = int.Parse(Console.ReadLine()); 
+                        Console.WriteLine("put the amount of time you want to spend brewing that tea (in seconds)");
+                        int readTime = int.Parse(Console.ReadLine());
+
+                        Tea
+                        break;
+                    case 5:
+                    case 6:
+
+
+                    default:
+                    Console.WriteLine("Incorrect number");
+                    break;
+                }
             }
-
-            List<Tea> teaList = new List<Tea>();
-
-            foreach (string line in splitText)
-            {
-                string[] splitLine = line.Split(',');
-                
-                teaList.Add(new Tea() { Name = splitLine[0], Kind = splitLine[1], BrewingTemperature = splitLine[2], BrewingTime = splitLine[3], Note = splitLine[3]  } );
-            }
-
-            Console.ReadKey();
-
 
         }
 
     }
 
-
-       
    }
--- BartoszNiezgodzkiApp/Tea.cs
+++ BartoszNiezgodzkiApp/Tea.cs
@@ -8,8 +8,41 @@ namespace BartoszNiezgodzkiApp
     {
         public string Name { get; set; }
         public string Kind { get; set; }
-        public string BrewingTemperature { get; set; }
-        public string BrewingTime { get; set; }
-        public string Note { get; set; }
+        public int BrewingTemperature { get; set; }
+        public int BrewingTime { get; set; }
+        public string SpecialFeature { get; set; }
+
+        public static List<Tea> WriteData (string[] data)
+        {
+            List<Tea> teaList = new List<Tea>();
+            foreach (string line in data)
+            {
+                string[] splitLine = line.Split(',');
+                if (splitLine[0][0] == '#' || splitLine.Length == 1) continue;
+                if (splitLine.Length == 5)
+                {
+                    teaList.Add(new Tea() { Name = splitLine[0], Kind = splitLine[1], BrewingTemperature = int.Parse(splitLine[2]), BrewingTime = int.Parse(splitLine[3]), SpecialFeature = splitLine[4] });
+                }
+                else if (splitLine.Length == 4)
+                {
+                    teaList.Add(new Tea() { Name = splitLine[0], Kind = splitLine[1], BrewingTemperature = int.Parse(splitLine[2]), BrewingTime = int.Parse(splitLine[3]) });
+                }
+                else
+                {
+                    Console.WriteLine("Wrong record length");
+                }
+
+            }
+
+            return teaList;
+        }
+
+        public static List<Tea> CheckTeaParameters (string[] data)
+        {
+            List<Tea> teaList = new List<Tea>();
+            
+
+            return teaList;
+        }
     }
 }
